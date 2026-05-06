2de5d2da9aeb358146f20b2b53fcf407d2c2054f
Fernando <fgutierrezara@gmail.com>
2026-05-06 18:24:52 +0200

    calibracion sensores

--- Core/Src/sensores/calibracion/calibracion.c
+++ Core/Src/sensores/calibracion/calibracion.c
@@ -2,12 +2,12 @@
 
 CalibrationData calibration =
 {
-    .throttle_min = 0,
-    .throttle_max = 4095,
+    .throttle_min = 1900,
+    .throttle_max = 2600,
 
-    .temp_engine_min = 100,
-    .temp_engine_max = 4095,
+    .temp_engine_min =0,
+    .temp_engine_max =4095,
 
-    .temp_air_min = 1700,
+    .temp_air_min = 0,
     .temp_air_max = 3500
 };
